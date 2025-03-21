0481c8685251663c0bc310f42556cc187473765d
zozoloco <zoe.chol111@gmail.com>
2025-03-21 12:26:27 -0400

    Modif titre script 1

--- Scripts/1_importer_taxo.R
+++ Scripts/1_importer_taxo.R
@@ -1,4 +1,11 @@
-#importation du fichier relatif à la taxonomie#
+##################################################
+# Fonction qui permet l'importation du fichier relatif à la taxonomie                                 
+#
+# Auteur: William Simard, Zoé Chol, Kyara Boisvert 
+# Date: 21-03-2025
+##################################################
+
+
 import.taxo <- function(){
   taxo <- read.csv("taxonomie.csv")
   return(taxo)
