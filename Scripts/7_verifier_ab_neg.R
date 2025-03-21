dcb2328745b1b8389fbc4b2c74e52fe80c491a0e
zozoloco <zoe.chol111@gmail.com>
2025-03-21 12:38:28 -0400

    Modif titre script 7

--- Scripts/7_verifier_ab_neg.R
+++ Scripts/7_verifier_ab_neg.R
@@ -1,4 +1,12 @@
-### Vérifier s'il y a des valeurs d'abondance négatives dans la colonne "values"
+##################################################
+# Fonction qui permet de vérifier les abondances négatives                         
+#
+# Auteur: William Simard, Zoé Chol, Kyara Boisvert 
+# Date: 21-03-2025
+##################################################
+
+
+# Le code suivant permet de vérifier s'il y a des valeurs d'abondance négatives dans la colonne "values".
 
 Verifier.abondance.negative <- function(negative){
   resultat <- any(negative$values < 0)
