65896b344869e7304f986d2656d107ff4b4c710a
zozoloco <zoe.chol111@gmail.com>
2025-03-21 12:41:42 -0400

    Modif titre script 9

--- Scripts/9_creer_col_notes.R
+++ Scripts/9_creer_col_notes.R
@@ -1,4 +1,14 @@
-#creer colonne notes#
+##################################################
+# Fonction qui permet de créer une colonne notes                        
+#
+# Auteur: William Simard, Zoé Chol, Kyara Boisvert 
+# Date: 21-03-2025
+##################################################
+
+
+# Le code suivant permet de créer une colonne notes pour gérer la presence de "?" 
+# dans $unit sans perdre l'information d'incertitude.
+
 creer.col.notes <- function(sansnotes){
   notes <- matrix(,nrow(sansnotes),1)
   colnames(notes) <- c("notes")
