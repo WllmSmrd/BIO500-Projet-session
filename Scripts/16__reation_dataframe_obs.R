ad323fba7acd7310c3f6f0f1df83eb5f22b791c1
zozoloco <zoe.chol111@gmail.com>
2025-03-27 14:32:54 -0400

    Modif nom + fonction script 16

---
+++ Scripts/16__reation_dataframe_obs.R
@@ -0,0 +1,13 @@
+##################################################
+# Fonction qui permet de créer  le dataframe des observations                  
+#
+# Auteur: William Simard, Zoé Chol, Kyara Boisvert 
+# Date: 21-03-2025
+##################################################
+
+
+creer.obs <- function(data){
+  dataframe3 <- data[,c("observed_scientific_name", "years", "unit", "values", 
+                        "coordo_x", "coordo_y", "notes")] #creation d'une matrice de 9 colonnes#
+  return(dataframe3)
+}
