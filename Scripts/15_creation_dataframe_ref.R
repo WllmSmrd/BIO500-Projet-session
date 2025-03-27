a93046d4afacf523d0a48b4cff2988a30534df56
zozoloco <zoe.chol111@gmail.com>
2025-03-27 14:29:48 -0400

    Modif nom + fonction script 15

---
+++ Scripts/15_creation_dataframe_ref.R
@@ -0,0 +1,13 @@
+##################################################
+# Fonction qui permet de créer  le dataframe des références                    
+#
+# Auteur: William Simard, Zoé Chol, Kyara Boisvert 
+# Date: 21-03-2025
+##################################################
+
+
+creer.ref <- function(data){
+  dataframe2<- data[,c("original_source", "creator", "title", "publisher", 
+                       "intellectual_rights", "license", "owner")]
+  return(dataframe2)
+}
--- Scripts/Creation_dataframe_ref.R
+++
@@ -1,8 +0,0 @@
-#Creation dataframe references#
-creer.ref <- function(data){
-  dataframe2<- data[,c("original_source", "creator", "title", "publisher", "intellectual_rights", "license", "owner")]
-  for (i in 1:7){
-    dataframe2[,i] <- as.character(dataframe2[,i])
-  }
-  return(dataframe2)
-}
