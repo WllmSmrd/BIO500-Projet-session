df207da6479a6f05d2824452e5aee389dd6f6c65
WllmSmrd <William.Simard2@usherbrooke.ca>
2025-04-19 12:32:13 -0400

    ajustement des figures 2 et 3

--- Scripts/25_creer_figures_2_3.R
+++ Scripts/25_creer_figures_2_3.R
@@ -9,12 +9,15 @@ creer.figures.2.3 <- function(data){
   
   #Figure 2 (Nombre d'observations total par classe)
   pdf("figure_2_obs_par_classe.pdf", width = 14, height = 6) #pour exportation de la figure en pdf
-  par(mfrow = c(1,1), mar = c(10, 6, 4, 2), mgp = c(4, 1, 0))                  # aucun fractionnement de la fenêtre graphique
+  par(mfrow = c(1,1), mar = c(10, 8, 4, 2), mgp = c(5, 1, 0))                  # aucun fractionnement de la fenêtre graphique
   
   total_par_classe <- data %>%         # Calcul du total d'observations par classe
     group_by(class) %>%
     summarise(total_obs = sum(Nb_obs)) %>%
     arrange(desc(total_obs))
+  
+  min_y <- floor(log10(min(total_par_classe$total_obs[total_par_classe$total_obs > 0])))
+  max_y <- ceiling(log10(max(total_par_classe$total_obs)))  # Calculer les bornes en log10
   
   barplot(total_par_classe$total_obs,
           names.arg = total_par_classe$class,
@@ -24,8 +27,10 @@ creer.figures.2.3 <- function(data){
           cex.names = 0.8,
           ylab = "Nombre total d'observations (log10)",
           main = "Observations par classe (échelle logarithmique)",
-          log = "y")                    # échelle logarithmique
-  
+          log = "y",                    # échelle logarithmique
+          ylim = c(10^min_y, 10^max_y)) 
+
+  #Ajoute manuellement le titre de l'axe des X
   mtext("Classe taxonomique", side = 1, line = 6)
   
   dev.off()
