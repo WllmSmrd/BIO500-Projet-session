dd17c46f77fd350ed6cf3cfc4429ab8420bbfec3
WllmSmrd <William.Simard2@usherbrooke.ca>
2025-04-20 14:05:13 -0400

    guide pour construction pipeline

--- organisation_pipeline_target.R
+++ organisation_pipeline_target.R
@@ -1,36 +1,41 @@
 ##ORGANISATION DU PIPELINE TARGET########################
 
+##Path
 
-##Importation données taxonomie
-#étape 1
+##Importation données 
+  #taxonomie
+    #target: étape 1
 
-
-##Importation données observations
-  #étape 2
-
-
-##Nettoyage données observations
-  #étapes 3 à 10 et 13
+  #observations
+   #target: étape 2
 
 
-##Nettoyage données taxonomie
-  #étapes 11, 12, 14
+##Nettoyage données
+  #observations
+    #target: étapes 3 à 10 et 13
 
+  #taxonomie
+    #étapes 11, 12, 14
 
-##Creation base de données SQL
+##Préparation des données observations pour injection
+  #target: étape 15
+  #target: étape 16
 
+##Creation base de données SQL et injection
+  #target: étapes abondances_bd (pas de # d'étape) et 17 à 19
+  #target: étapes 20-21
 
 ##Extraction de données pour l'analyse
-  ###données biodiversité
-    #étape 22
+  #données biodiversité
+    #target: étape 22
 
-  ###données observations par taxons
-    #étape 23
+  #données observations par taxons
+    #target: étape 23
 
 
-##Creation des figures pour l'analyse
-  ###Figure 1
+##Analyses et visualisation
+  #Figure 1
     #étape 24
   
-  ###Figures 2-3
+  #Figures 2-3
     #étape 25
