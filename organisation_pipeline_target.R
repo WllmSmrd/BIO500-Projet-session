1b890fd31791ac3d5a9183ecb606ccff939bed00
WllmSmrd <William.Simard2@usherbrooke.ca>
2025-04-20 13:01:20 -0400

    pour nous aider a l'organisation du pipeline

---
+++ organisation_pipeline_target.R
@@ -0,0 +1,36 @@
+##ORGANISATION DU PIPELINE TARGET########################
+
+
+##Importation données taxonomie
+#étape 1
+
+
+##Importation données observations
+  #étape 2
+
+
+##Nettoyage données observations
+  #étapes 3 à 10 et 13
+
+
+##Nettoyage données taxonomie
+  #étapes 11, 12, 14
+
+
+##Creation base de données SQL
+
+
+##Extraction de données pour l'analyse
+  ###données biodiversité
+    #étape 22
+
+  ###données observations par taxons
+    #étape 23
+
+
+##Creation des figures pour l'analyse
+  ###Figure 1
+    #étape 24
+  
+  ###Figures 2-3
+    #étape 25
